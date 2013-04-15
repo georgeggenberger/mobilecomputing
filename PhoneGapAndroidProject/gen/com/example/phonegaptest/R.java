375f6ced924a161e2901a2ad9180cff6c6b06479
albertliebmann <albertliebmann@gmx.net>
2013-04-15 19:54:27 +0200

    Deleted unneeded resources

--- PhoneGapAndroidProject/gen/com/example/phonegaptest/R.java
+++ PhoneGapAndroidProject/gen/com/example/phonegaptest/R.java
@@ -13,23 +13,14 @@ public final class R {
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
-    public static final class id {
-        public static final int action_settings=0x7f090000;
-    }
-    public static final class layout {
-        public static final int activity_phone_gap=0x7f030000;
-    }
-    public static final class menu {
-        public static final int phone_gap=0x7f080000;
-    }
     public static final class string {
-        public static final int app_name=0x7f060000;
+        public static final int app_name=0x7f050000;
     }
     public static final class style {
         /** 
@@ -42,13 +33,13 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f060001;
     }
     public static final class xml {
-        public static final int config=0x7f040000;
+        public static final int config=0x7f030000;
     }
 }
