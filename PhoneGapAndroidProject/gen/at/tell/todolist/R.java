9ec8a8347ec8da870e6346868c6afb656e48ef9c
albertliebmann <albertliebmann@gmx.net>
2013-05-14 21:01:54 +0200

    Added short application description to resources + Manifest.

--- PhoneGapAndroidProject/gen/at/tell/todolist/R.java
+++ PhoneGapAndroidProject/gen/at/tell/todolist/R.java
@@ -20,6 +20,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class string {
+        public static final int app_description=0x7f050001;
         public static final int app_name=0x7f050000;
     }
     public static final class style {
